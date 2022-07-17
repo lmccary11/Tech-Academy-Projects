215d0ef2684cb35b24938bc4d10ae190f59bc3d2
Lawrence McCary <98392880+lmccary11@users.noreply.github.com>
2022-07-17 13:01:04 -0400

    Website Assignment

---
+++ VStudio/WebAppAssignment/WebAppAssignment/wwwroot/js/site.js
@@ -0,0 +1,25 @@
+﻿function myMove() {
+    let id = null;
+    const elem = document.getElementById("Animation");
+    let pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 5);
+    function frame() {
+        if (pos == 2360) {
+            clearInterval(id);
+        } if (pos <=489) {
+            pos++;
+            elem.style.top = pos + "px";
+            
+        } if (489 < pos & pos <= 1180) {
+            pos++;            
+            elem.style.left = pos - 490 + "px";
+        } if (1670 >= pos & pos > 1180) {
+            pos++;
+            elem.style.top = 1670 -  pos + "px";
+        } if (2360 > pos & pos> 1670) {
+            pos++;
+            elem.style.left = 2360 - pos + "px";
+        }
+    }
+}
---
+++ VStudio/WebAppTutorial/WebAppTutorial/wwwroot/js/site.js
@@ -0,0 +1,7 @@
+﻿// Functions to be called onmouseover and onmouseout to change the color of the "Hello World"
+function newColor() {
+    document.getElementById("hello_world").style.color = "blue";
+}
+function oldColor() {
+    document.getElementById("hello_world").style.color = "darkolivegreen";
+}
